a9d8ecc88094f723ecad15a76ae2217f248b9f61
Chijindu <cnnwakama@gmail.com>
2020-12-23 18:55:44 +0100

    Added a search btn

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -20,16 +20,25 @@ this.setState({text: e.target.value})
   render(){
     return(
       <div>
-           <form onSubmit={this.submitHandler} style={{border: '2px solid black',marginTop:'15px'}}>
-               Enter Video Search:
+         <form onSubmit={this.submitHandler} style={{border: '2px solid black',marginTop:'15px'}}>
+               
+           <div style={{display:'flex',flexWrap:'nowrap',alignItems:'center',gap: '15px'}}>
                 <div style={{width: '80%'}} className="input-field inline">
                   <label htmlFor="search"></label>
                   <input id="search" 
                    type="text"
+                   placeholder="Search youtube..."
                    value={this.state.text}
                    onChange={this.inputChange}/> 
                  </div>
-            </form>
+
+                 <div>
+                 <button className="btn grey darken-2 waves-effect waves-light" type="submit" name="action">
+                  <i className="material-icons">search</i>
+                  </button>
+                </div>
+            </div>
+          </form>
         </div>    )
   }
 }
